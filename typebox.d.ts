4aa00fb16570a9d3e01ee4dec25b0918fc825778
Adrian Kriegel <adrian@unolog.in>
2022-01-13 23:34:20 +0100

    using ./ as build output dir as ts-jest was getting confused

---
+++ typebox.d.ts
@@ -0,0 +1,4 @@
+import ExtendedTypeBuilder from './main';
+import * as Typebox from '@sinclair/typebox';
+export declare const Type: ExtendedTypeBuilder;
+export declare type Static<T extends Typebox.TSchema> = Typebox.Static<T>;
