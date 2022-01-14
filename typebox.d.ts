807fa73f9c113c8e8f27b88f96354c6eebf3d6e5
Adrian Kriegel <adrian@unolog.in>
2022-01-14 09:50:09 +0100

    ignoring build output

--- typebox.d.ts
+++
@@ -1,4 +0,0 @@
-import ExtendedTypeBuilder from './main';
-import * as Typebox from '@sinclair/typebox';
-export declare const Type: ExtendedTypeBuilder;
-export declare type Static<T extends Typebox.TSchema> = Typebox.Static<T>;
